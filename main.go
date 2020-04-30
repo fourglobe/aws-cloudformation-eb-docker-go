290f999b40dc3c1afc10b9d7849e619576bca019
Bumin Kim <kim_bumin@fourglobe.co.jp>
2020-04-30 20:30:29 +0900

    fix: elb health check.

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	/// !WARN: don't touch me.
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(204, nil)
+	})
+
+	// mapping url.
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
