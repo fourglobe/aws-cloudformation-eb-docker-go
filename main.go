345784ebe509fe70223b842cb80e883b7d4b1daa
Bumin Kim <kim_bumin@fourglobe.co.jp>
2020-05-01 07:04:47 +0900

    update: path

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	/// !WARN: don't touch me.
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(204, nil)
-	})
-
-	// mapping url.
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.GET("", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"health": "ok",
 		})
 	})
 	r.Run()
